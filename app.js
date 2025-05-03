f37316a2df939d49e327e19e96c02a2445023f3e
JuliaSmolianska <i.julia.i1990@gmail.com>
2025-05-03 19:24:31 +0300

    update

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
-import swaggerUi from 'swagger-ui-express';
 import fs from 'fs';
 import clientsRouter from './routes/api/clients_router.js';
 import userRouter from "./routes/api/users_router.js";
 import authRouter from "./routes/api/auth_router.js";
-
-const swaggerJson = JSON.parse(
-  fs.readFileSync(`./swagger.json`)
-);
 
 const app = express();
 
@@ -19,11 +14,6 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerJson)
-);
 
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
